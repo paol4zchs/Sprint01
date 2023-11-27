d26b035edbf333e0d3b40ac05d13b1668cb75745
paol4zchs <133895456+paol4zchs@users.noreply.github.com>
2023-11-27 15:49:45 -0300

    Add files via upload

---
+++ prjEmpresa_pt01/src/main/java/com/example/paola/PrjEmpresa/controller/DepartamentoController.java
@@ -0,0 +1,84 @@
+package com.example.paola.PrjEmpresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.paola.PrjEmpresa.entities.Departamento;
+import com.example.paola.PrjEmpresa.services.DepartametoService;
+
+
+import jakarta.validation.Valid;
+
+@Tag( name = "Departamentos", description = "API REST DE GERECIAMENTO DE DEPARTAMENTOS")
+@RestController
+@RequestMapping("/departamento")
+public class DepartamentoController {
+	
+
+	private final DepartametoService departametoService;
+
+	@Autowired
+	public DepartamentoController(DepartametoService departametoService) {
+		this.departametoService = departametoService;
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary= "Localiza departamento por ID")
+	public ResponseEntity<Departamento> findDepartamentobyId(@PathVariable Long depcodigo) {
+		Departamento Departamento = departametoService.findDepartamentoById(depcodigo);
+		if (Departamento != null) {
+			return ResponseEntity.ok(Departamento);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/")
+	@Operation(summary= "Apresenta todos os departamentos")
+	public ResponseEntity<List<Departamento>> findAllDepartamentocontrol() {
+		List<Departamento> departamentos = departametoService.findAllDepartamento();
+		return ResponseEntity.ok(departamentos);
+	}
+
+	@PostMapping("/")
+	@Operation(summary= "Cadastra um departamento")
+	public ResponseEntity<Departamento> insertDepartamentoControl(@RequestBody @Valid Departamento departamento) {
+		Departamento novoDepartamento= departametoService.insertDepartamento(departamento);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoDepartamento);
+	}
+
+	@PutMapping("/id")
+	@Operation(summary= "Altera um departamento")
+	public ResponseEntity<Departamento> updateDepartamentoControl(@PathVariable Long depcodigo, @RequestBody @Valid Departamento departamento) {
+		Departamento mudadepartamento = departametoService.updateDepartamento(depcodigo, departamento);
+		if (mudadepartamento != null) {
+			return ResponseEntity.ok(departamento);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/id")
+	@Operation(summary= "Exclui um departamento")
+	public ResponseEntity<String> deleteDepartamentoControl(@PathVariable Long depcodigo) {
+		boolean remover = departametoService.deleteDepartamento(depcodigo);
+		if (remover) {
+			return ResponseEntity.ok().body("departamento Excluido com sucesso");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
+}
---
+++ prjEmpresa_pt01/src/main/java/com/example/paola/PrjEmpresa/controller/FuncionarioController.java
@@ -0,0 +1,83 @@
+package com.example.paola.PrjEmpresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.paola.PrjEmpresa.entities.Funcionario;
+import com.example.paola.PrjEmpresa.services.FuncionarioService;
+
+import jakarta.validation.Valid;
+
+@Tag(name = "Funcionarios", description = "API REST DE GERECIAMENTO DE Funcionarios")
+@RestController
+@RequestMapping("/funcionario")
+public class FuncionarioController {
+	
+
+	private final FuncionarioService funcionarioService;
+
+	@Autowired
+	public FuncionarioController(FuncionarioService funcionarioService) {
+		this.funcionarioService = funcionarioService;
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary= "Localiza funcionario por ID")
+	public ResponseEntity<Funcionario> findFuncionariobyId(@PathVariable Long id) {
+		Funcionario Funcionario = funcionarioService.findFuncionarioById(id);
+		if (Funcionario != null) {
+			return ResponseEntity.ok(Funcionario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/")
+	@Operation(summary= "Apresenta todos os funcionarios")
+	public ResponseEntity<List<Funcionario>> findAllFuncionarioscontrol() {
+		List<Funcionario> funcionarios = funcionarioService.findAllFuncionario();
+		return ResponseEntity.ok(funcionarios);
+	}
+
+	@PostMapping("/")
+	@Operation(summary= "Cadastra um funcionario")
+	public ResponseEntity<Funcionario> insertFuncionarioControl(@RequestBody @Valid Funcionario funcionario) {
+		Funcionario novoFuncionario= funcionarioService.insertFuncionario(funcionario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoFuncionario);
+	}
+
+	@PutMapping("/id")
+	@Operation(summary= "Altera um funcionario")
+	
+	public ResponseEntity<Funcionario> updateUsuarioControl(@PathVariable Long funcodigo, @RequestBody @Valid Funcionario funcionario) {
+		Funcionario mudafuncionario = funcionarioService.updateFuncionario(funcodigo, funcionario);
+		if (mudafuncionario != null) {
+			return ResponseEntity.ok(funcionario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/id")
+	@Operation(summary= "Exclui um funcionario")
+	public ResponseEntity<String> deleteFuncionarioControl(@PathVariable Long funcodigo) {
+		boolean remover = funcionarioService.deleteFuncionario(funcodigo);
+		if (remover) {
+			return ResponseEntity.ok().body("funcionario Excluido com sucesso");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+}
