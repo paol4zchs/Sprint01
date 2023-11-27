b0ad139704f85767818b33352ffd335cb9c5afb9
paol4zchs <133895456+paol4zchs@users.noreply.github.com>
2023-11-27 15:48:17 -0300

    Delete prjEmpresa_pt01 directory

--- prjEmpresa_pt01/src/main/java/com/example/paola/PrjEmpresa/repositories/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.paola.PrjEmpresa.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.paola.PrjEmpresa.entities.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-
-}
